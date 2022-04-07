cfc257624b0ac1535d550ae0998de72de3ac0e02
RoSteik <rostyslav.postolaki@gmail.com>
2022-04-07 15:51:24 +0300

    add laba_3_code java

--- src/main/java/iot/lviv/ua/FruitBox.java
+++ src/main/java/iot/lviv/ua/FruitBox.java
@@ -1,4 +1,35 @@
 package iot.lviv.ua;
 
-public class FruitBox extends FruitShop{
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FruitBox extends FruitShop {
+
+    public enum ripeningSeasonEnum{
+        SUMMER,
+        AUTUMN,
+        WINTER,
+        SPRING
+    }
+
+    private  String nameOfFruitInBox;
+    private  String colourOfFruitInBox;
+    private ripeningSeasonEnum ripeningSeason;
+    private  int price; //per kilogram
+
+    public FruitBox(String nameOfFruitInBox, String colourOfFruitInBox,ripeningSeasonEnum ripeningSeason, int price) {
+        this.nameOfFruitInBox = nameOfFruitInBox;
+        this.colourOfFruitInBox = colourOfFruitInBox;
+        this.ripeningSeason = ripeningSeason;
+        this.price = price;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.nameOfFruitInBox + " " +  this.colourOfFruitInBox + " "
+                + this.ripeningSeason + " , Price - " +  this.price;
+    }
 }
